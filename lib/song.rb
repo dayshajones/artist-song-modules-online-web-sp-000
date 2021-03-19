f8340694f7f93ecc15f9cc51269550fc1a6d8516
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 21:35:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,14 @@
 require 'pry'
 
 class Song
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  # def initialize
-  #   self.class.all << self
-  # end
+  def initialize
+    @@songs << self
+  end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -23,11 +18,19 @@ class Song
     @@songs
   end
 
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
   def artist=(artist)
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
